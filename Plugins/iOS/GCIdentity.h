862b44cebbd8f266305ec2081409170eb62766b2
A. Bivzyuk <im.alive.work@gmail.com>
2017-10-30 11:22:16 +0200

    Remove comment

--- Plugins/iOS/GCIdentity.h
+++ Plugins/iOS/GCIdentity.h
@@ -1,11 +1,3 @@
-//
-//  GCIdentity.h
-//  GCIdentity
-//
-//  Created by Alexander Bivzyuk on 10/27/17.
-//  Copyright © 2017 Alexander Bivzyuk. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import <GameKit/GameKit.h>
 
